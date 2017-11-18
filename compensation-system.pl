e3e8c93745d32bfaf7335d95efa55a6526b7382d
Hagar Yasser <hagar.y.omar@gmail.com>
2017-11-18 22:41:52 +0200

    member_times fixed

--- compensation-system.pl
+++ compensation-system.pl
@@ -44,108 +44,108 @@ compensate(IN, OUT):-
   RoomCapacity #>= GroupSize - 10,
 
   %----------------------------LABELING------------------------------
-  cost(TAOff, GroupOff, (RoomCapacity,GroupSize), CompStart, Preferences, OUT, C), 
-  labeling([min(C)], [CompSlot, CompDay, CompWeek, CompRoom]),
-  write(C).  
+  cost(TAOff, GroupOff, (RoomCapacity,GroupSize), CompStart, Preferences, OUT, C),
+  labeling([ffc, min(C)], [CompSlot, CompDay, CompWeek, CompRoom]),
+  write(C).
 
+  %--------------------------------------------------------------------------------------
+  %--------------------------------------------------------------------------------------
 
- cost(TAOff, GroupOff, Room, CompStart, Preferences, OUT, C):-
- OUT = (CompWeek, CompDay, CompSlot, CompRoom, RoomLoc),
- Preferences = (Times,Places),
- Room  = (RoomCapacity, GroupSize),
- cost_of_TA(TAOff,CompDay,C1),
- cost_of_Group(GroupOff,CompDay,C2),
- cost_of_time(CompStart,Times,(CompWeek,CompDay,CompSlot),C3),  
- cost_of_Loc(Places,RoomLoc,C4),
- cost_of_Capacity(RoomCapacity,GroupSize,C5),
-C #= C1 + C2 + C3 + C4 + C5.
+cost(TAOff, GroupOff, Room, CompStart, Preferences, OUT, C):-
+  OUT = (CompWeek, CompDay, CompSlot, CompRoom, RoomLoc),
+  Preferences = (Times, Places),
+  Room  = (RoomCapacity, GroupSize),
+  cost_of_TA(TAOff,CompDay,C1),
+  cost_of_Group(GroupOff,CompDay,C2),
+  cost_of_time(CompStart,Times,(CompWeek,CompDay,CompSlot),C3),
+  cost_of_Loc(Places,RoomLoc,C4),
+  cost_of_Capacity(RoomCapacity,GroupSize,C5),
+  C #= C1 + C2 + C3 + C4 + C5.
 
 
 
 valid_date(StartWeek, StartDay, CompWeek, CompDay):-
-   StartWeek #= CompWeek,
-  CompDay #>= StartDay.
+StartWeek #= CompWeek,
+CompDay #>= StartDay.
 valid_date(StartWeek, StartDay, CompWeek, CompDay):-
-  CompWeek #> StartWeek.
+CompWeek #> StartWeek.
 
 validate_room_type(1,1).
 validate_room_type(_,PrefRoomType):-
-  PrefRoomType #\= 1.
+PrefRoomType #\= 1.
 
 
 
 not_member(_, []).
 
 not_member((X,Y), L ):-
-  L = [(X1,Y1) | T],
-  Y #\= Y1,
-  not_member((X,Y) , T).
+L = [(X1,Y1) | T],
+Y #\= Y1,
+not_member((X,Y) , T).
 
 not_member((X,Y), L ):-
-  L = [(X1,Y1) | T],
-  X #\= X1,
-  not_member((X,Y) , T).
+L = [(X1,Y1) | T],
+X #\= X1,
+not_member((X,Y) , T).
 
 
 
 %Calculate Cost of the Week Day according to the days off of TA and Group.
 
 cost_of_TA(Arr,Element,C):-
-  element(_,Arr,Element),
-  C #= 10.
+element(_,Arr,Element),
+C #= 10.
 
 cost_of_TA(Arr,Element,C):-
-  C #= 0.
- 
-cost_of_Group(Arr,Element,C):-
-  element(_,Arr,Element),
-  C #= 5.
+C #= 0.
 
 cost_of_Group(Arr,Element,C):-
-  C #= 0.
+element(_,Arr,Element),
+C #= 5.
+
+cost_of_Group(Arr,Element,C):-
+C #= 0.
 
 % Calculate the cost of the preference day compared to the found day.
 
 
-member_times(X1,X2,[(D1,S1)|T],(D2,S2)):-
-    X1 #= X2,
-    D1 #= D2,
-    S1 #= S2.
+member_times(1, [(D1,S1)|T],(D2,S2)):-
+  D1 #= D2,
+  S1 #= S2.
 
-member_times(X,X1,[(D1,S1)|T],(D,S)):-
-(S1 #\= S;
-D1 #\= D),
-X1 #= X + 1,
-member_times(X1,X2,T,(D,S)).
+member_times(X, [(D1,S1)|T],(D,S)):-
+  X #> 1,
+  X1 #= X - 1,
+  member_times(X1,T,(D,S)).
 
-% Calculate diff between two dates 
+% Calculate diff between two dates
 
 date_diff((W,D),(CompW,CompD),Diff):-
-Diff #= 6*(CompW-W)+(CompD-D).
+  Diff #= 6*(CompW-W)+(CompD-D).
 
 
 
 cost_of_time((StartW, StartD), Times, (CompWeek, CompD, CompSlot),C):-
-    write('hello'),
-    StartW #= CompWeek,
-member_times(1,C,Times,(CompD, CompSlot)).
+  write('prefered cost '),
+  StartW #= CompWeek,
+  member_times(C,Times,(CompD, CompSlot)).
 
 cost_of_time((StartW,StartD),Times,T,C):-
-T = (CompW,CompD, _),
-length(Times,N),
-date_diff((StartW,StartD),(CompW,CompD),C1),
-C #= (C1+1) * (N+1).
+  write('difference cost '),
+  T = (CompW,CompD, _),
+  length(Times,N),
+  date_diff((StartW,StartD),(CompW,CompD),C1),
+  C #= (C1+1) * (N+1).
 
 cost_of_Loc(PrefLoc,RoomLoc,C):-
-  element(C,PrefLoc,RoomLoc).
+element(C,PrefLoc,RoomLoc).
 
 % Calculate the cost of the room capacity.
 
 cost_of_Capacity(RoomCapacity,GroupSize,C):-
-  RoomCapacity #< GroupSize,
-  C1 #= GroupSize - RoomCapacity,
-  C  #= C1*10.
+RoomCapacity #< GroupSize,
+C1 #= GroupSize - RoomCapacity,
+C  #= C1*10.
 
 cost_of_Capacity(RoomCapacity,GroupSize,C):-
   C #= RoomCapacity - GroupSize.
-
